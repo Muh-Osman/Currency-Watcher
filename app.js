e5e28602fb44b8d4fabe75d74524969fc4121b58
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-11-05 15:42:02 +0300

    fix

--- app.js
+++ app.js
@@ -7,15 +7,38 @@ fetch('https://api.currencyfreaks.com/latest?apikey=4436bcb894744cca88efe65f4232
         const sar = document.querySelector('.sar-num');
         const egp = document.querySelector('.egp-num');
         const aed = document.querySelector('.aed-num');
+        const jod = document.querySelector('.jod-num');
+        const lyd = document.querySelector('.lyd-num');
+        const kwd = document.querySelector('.kwd-num');
+        const qar = document.querySelector('.qar-num');
+        const bhd = document.querySelector('.bhd-num');
+        const omr = document.querySelector('.omr-num');
         const eur = document.querySelector('.eur-num');
-
+        const cad = document.querySelector('.cad-num');
+        const gbp = document.querySelector('.gbp-num');
+        const tryy = document.querySelector('.try-num');
+        const sek = document.querySelector('.sek-num');
+        const nok = document.querySelector('.nok-num');
+        const dkk = document.querySelector('.dkk-num');
 
 
 
         sar.innerHTML = Number.parseFloat(currency.rates['SAR']).toFixed(3);
         egp.innerHTML = Number.parseFloat(currency.rates['EGP']).toFixed(3);
         aed.innerHTML = Number.parseFloat(currency.rates['AED']).toFixed(3);
+        jod.innerHTML = Number.parseFloat(currency.rates['JOD']).toFixed(3);
+        lyd.innerHTML = Number.parseFloat(currency.rates['LYD']).toFixed(3);
+        kwd.innerHTML = Number.parseFloat(currency.rates['KWD']).toFixed(3);
+        qar.innerHTML = Number.parseFloat(currency.rates['QAR']).toFixed(3);
+        bhd.innerHTML = Number.parseFloat(currency.rates['BHD']).toFixed(3);
+        omr.innerHTML = Number.parseFloat(currency.rates['OMR']).toFixed(3);
         eur.innerHTML = Number.parseFloat(currency.rates['EUR']).toFixed(3);
+        cad.innerHTML = Number.parseFloat(currency.rates['CAD']).toFixed(3);
+        gbp.innerHTML = Number.parseFloat(currency.rates['GBP']).toFixed(3);
+        tryy.innerHTML = Number.parseFloat(currency.rates['TRY']).toFixed(3);
+        sek.innerHTML = Number.parseFloat(currency.rates['SEK']).toFixed(3);
+        nok.innerHTML = Number.parseFloat(currency.rates['NOK']).toFixed(3);
+        dkk.innerHTML = Number.parseFloat(currency.rates['DKK']).toFixed(3);
 
 
 
@@ -32,20 +55,56 @@ fetch('https://api.currencyfreaks.com/latest?apikey=4436bcb894744cca88efe65f4232
             var flotThree = input.value * currency.rates['AED'];
             aed.innerHTML = Number.parseFloat(flotThree).toFixed(3);
 
-            var flotFour = input.value * currency.rates['EUR'];
-            eur.innerHTML = Number.parseFloat(flotFour).toFixed(3);
+            var flotFour = input.value * currency.rates['JOD'];
+            jod.innerHTML = Number.parseFloat(flotFour).toFixed(3);
 
+            var flotFive = input.value * currency.rates['LYD'];
+            lyd.innerHTML = Number.parseFloat(flotFive).toFixed(3);
 
+            var flotSix = input.value * currency.rates['KWD'];
+            kwd.innerHTML = Number.parseFloat(flotSix).toFixed(3);
 
+            var flotSeven = input.value * currency.rates['QAR'];
+            qar.innerHTML = Number.parseFloat(flotSeven).toFixed(3);
 
+            var flotEight = input.value * currency.rates['BHD'];
+            bhd.innerHTML = Number.parseFloat(flotEight).toFixed(3);
+
+            var flotNine = input.value * currency.rates['OMR'];
+            omr.innerHTML = Number.parseFloat(flotNine).toFixed(3);
+
+            var flotTeen = input.value * currency.rates['SEK'];
+            sek.innerHTML = Number.parseFloat(flotTeen).toFixed(3);
+
+            var flotEleven = input.value * currency.rates['NOK'];
+            nok.innerHTML = Number.parseFloat(flotEleven).toFixed(3);
+
+            var flotTwilve = input.value * currency.rates['DKK'];
+            dkk.innerHTML = Number.parseFloat(flotTwilve).toFixed(3);
+
+            var flotTherteen = input.value * currency.rates['EUR'];
+            eur.innerHTML = Number.parseFloat(flotTherteen).toFixed(3);
+
+            var flotFourteen = input.value * currency.rates['CAD'];
+            cad.innerHTML = Number.parseFloat(flotFourteen).toFixed(3);
+
+            var flotFiveteen = input.value * currency.rates['GBP'];
+            gbp.innerHTML = Number.parseFloat(flotFiveteen).toFixed(3);
+
+            var flotSixteen = input.value * currency.rates['TRY'];
+            tryy.innerHTML = Number.parseFloat(flotSixteen).toFixed(3);
         });
+
+
+        // Last Update
+        let ti = document.querySelector('.ti');
+        ti.innerHTML = currency.date;
 
 
     });
 
 
-var d = new Date(); // for now
-d.getHours(); // => 9
-d.getMinutes(); // =>  30
-d.getSeconds(); // => 51
+
+
+
 
