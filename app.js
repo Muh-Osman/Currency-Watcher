55c1eb0bad0692f3e1b6c1c07b0076db63105957
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-11-05 23:41:30 +0300

    fix

--- app.js
+++ app.js
@@ -114,8 +114,114 @@ fetch('https://api.currencyfreaks.com/latest?apikey=4436bcb894744cca88efe65f4232
 
 
 
-        // Save data in local storage to compare it
-        window.localStorage.setItem("SAR", currency.rates['SAR'])
+        // Set Data in local storage to compare it after 3s
+        setTimeout(function () {
+            for (var key in currency.rates) {
+                if (currency.rates.hasOwnProperty(key)) {
+                    localStorage.setItem(key, currency.rates[key]);
+                }
+            }
+        }, 3000);
+
+
+
+        // Get Data
+        var up = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg>`;
+        var down = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>`;
+        var equal = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blue" class="bi bi-dash-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>`;
+
+
+
+        var oldA = window.localStorage.getItem('SAR');
+        var arrowsar = document.querySelector('.arrowsar');
+
+        if (oldA > currency.rates['SAR']) {
+            arrowsar.innerHTML = up;
+
+        } else if (oldA < currency.rates['SAR']) {
+
+            arrowsar.innerHTML = down;
+
+        } else {
+            arrowsar.innerHTML = equal;
+        }
+
+
+
+        var oldB = window.localStorage.getItem('EGP');
+        var arrowegp = document.querySelector('.arrowegp');
+
+        if (oldB > currency.rates['EGP']) {
+            arrowegp.innerHTML = up;
+
+        } else if (oldB < currency.rates['EGP']) {
+
+            arrowegp.innerHTML = down;
+
+        } else {
+            arrowegp.innerHTML = equal;
+        }
+
+
+
+        var oldC = window.localStorage.getItem('AED');
+        var arrowaed = document.querySelector('.arrowaed');
+
+        if (oldC > currency.rates['AED']) {
+            arrowaed.innerHTML = up;
+
+        } else if (oldC < currency.rates['AED']) {
+
+            arrowaed.innerHTML = down;
+
+        } else {
+            arrowaed.innerHTML = equal;
+        }
+
+
+        var oldD = window.localStorage.getItem('JOD');
+        var arrowjod = document.querySelector('.arrowjod');
+
+        if (oldD > currency.rates['JOD']) {
+            arrowjod.innerHTML = up;
+
+        } else if (oldD < currency.rates['JOD']) {
+
+            arrowjod.innerHTML = down;
+
+        } else {
+            arrowjod.innerHTML = equal;
+        }
+
+
+        var oldE = window.localStorage.getItem('LYD');
+        var arrowlyd = document.querySelector('.arrowlyd');
+
+        if (oldE > currency.rates['LYD']) {
+            arrowlyd.innerHTML = up;
+
+        } else if (oldE < currency.rates['LYD']) {
+
+            arrowlyd.innerHTML = down;
+
+        } else {
+            arrowlyd.innerHTML = equal;
+        }
+
+
+        var oldF = window.localStorage.getItem('KWD');
+        var arrowkwd = document.querySelector('.arrowkwd');
+
+        if (oldF > currency.rates['KWD']) {
+            arrowkwd.innerHTML = up;
+
+        } else if (oldF < currency.rates['KWD']) {
+
+            arrowkwd.innerHTML = down;
+
+        } else {
+            arrowkwd.innerHTML = equal;
+        }
 
 
 
